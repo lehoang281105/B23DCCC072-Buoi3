90e49584b949d48063568f80bc9283f35f8ddef9
lehoang281105 <lehoang281105@gmail.com>
2024-11-21 11:16:02 +0700

    lehoang281105

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import ProductTable from "./components/ProductTable";
+
+const App: React.FC = () => {
+  return (
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+      <ProductTable />
+    </div>
+  );
+};
+
+export default App;
